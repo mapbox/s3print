c0ca173eb59b08b2610cd0383dd2b40ffc6e10e7
Ryan Clark <ryan.clark.j@gmail.com>
2015-04-03 11:29:11 -0700

    print objects under some prefix

--- bin/s3print.js
+++ bin/s3print.js
@@ -5,31 +5,82 @@ var url = require('url');
 var _ = require('lodash');
 
 function usage(code) {
-  console.log('s3print <s3 uri>\n');
+  console.log('s3print <s3 uri> [--prefix]\n');
   console.log('For example:');
   console.log('  s3print s3://my-bucket/path/to/some/file.txt');
+  console.log('  s3print  --prefix s3://my-bucket/some/prefix');
   process.exit(code);
 }
 
-if (!process.argv[2]) usage(1);
-if (process.argv[2] === '--help' || process.argv[2] === '-h') usage(0);
+var args = require('minimist')(process.argv.slice(2));
+if (!args._[0] && !args.prefix) usage(1);
+if (!args._[0] && args.prefix) args._[0] = args.prefix;
+if (args.help) usage(0);
 
-var uri = url.parse(process.argv[2]);
+var uri = url.parse(args._[0]);
 var s3 = new AWS.S3();
 
 s3.config.getCredentials(function(err) {
   if (err && err.message !== 'Could not load credentials from any providers') throw err;
-  request = err ?
+  var get = err ?
     _.partial(s3.makeUnauthenticatedRequest.bind(s3), 'getObject') :
     s3.getObject.bind(s3);
+  var list = err ?
+    _.partial(s3.makeUnauthenticatedRequest.bind(s3), 'listObjects') :
+    s3.listObjects.bind(s3);
 
-  request({
-    Bucket: uri.hostname,
-    Key: uri.pathname.slice(1)
-  }).createReadStream().on('error', function(err) {
-    console.error(err);
-    process.exit(1);
-  }).on('end', function() {
-    process.stdout.write('\n');
-  }).pipe(process.stdout);
+  if (!args.prefix) {
+    return get({
+      Bucket: uri.hostname,
+      Key: uri.pathname.slice(1)
+    }).createReadStream().on('error', function(err) {
+      console.error(err);
+      process.exit(1);
+    }).on('end', function() {
+      process.stdout.write('\n');
+    }).pipe(process.stdout);
+  }
+
+  ls();
+
+  function ls(marker) {
+    var params = {
+      Bucket: uri.hostname,
+      Prefix: uri.pathname.slice(1)
+    };
+    if (marker) params.Marker = marker;
+    list(params, function(err, data) {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+
+      var last = data.Contents.slice(-1)[0];
+
+      getSome(data.Contents, function(err) {
+        if (err) {
+          console.error(err);
+          process.exit(1);
+        }
+
+        if (data.IsTruncated && last) return ls(last.Key);
+        process.exit(0);
+      });
+    });
+  }
+
+  function getSome(objects, callback) {
+    var object = objects.shift();
+    if (!object) return callback();
+    get({
+      Bucket: uri.hostname,
+      Key: object.Key
+    }).createReadStream()
+      .once('error', callback)
+      .on('end', function() {
+        process.stdout.write('\n');
+        getSome(objects, callback);
+      })
+      .pipe(process.stdout);
+  }
 });
--- readme.md
+++ readme.md
@@ -9,12 +9,15 @@ $ npm install -g s3print
 
 ## Configuration
 
-Provide S3 credentials through any means [supported by the AWS JS SDK](http://docs.aws.amazon.com/AWSJavaScriptSDK/guide/node-configuring.html). If the file is publicly accessible, you do not need to provide any credentials.
+Provide S3 credentials through any means [supported by the AWS JS SDK](http://docs.aws.amazon.com/AWSJavaScriptSDK/guide/node-configuring.html). If the file is publicly accessible, you do not need to provide any credentials. If you use the `--prefix` option you'll need permission to `ListBucket` in the appropriate bucket.
 
 ## Usage
 ```sh
 $ s3print s3://s3print-test/test-file
 # Prints "Hello world!"
+
+$ s3print --prefix s3://s3print-test/
+# Lists object under the given prefix and prints them all
 ```
 
 ## Alternative
